1636e79e15e5468e4f613d199bbcae737c7b6b1c
Miguel Leite <miguelleite200leite@gmail.com>
2022-06-04 17:40:54 +0100

    🚀 add new routers

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',[PagesController::class,'index']);
+Route::get('/',[PagesController::class,'index'])->name('pages.index');
+Route::get('/recuperar-senha',[PagesController::class,'forgotPassword'])->name('pages.forgotPassword');
