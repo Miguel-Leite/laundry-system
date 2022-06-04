983ae0eb2c2aef02eab02978af8675fdf4bce307
Miguel Leite <miguelleite200leite@gmail.com>
2022-06-04 23:56:33 +0100

    ✅ config send mail user

---
+++ app/Mail/UserSendMail.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class UserSendMail extends Mailable
+{
+    use Queueable, SerializesModels;
+    /**
+     *  variables: $user, $view 
+     *
+     * @var [private]
+     */
+    private $user,$options;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user, array $options)
+    {
+        $this->user = $user;
+        $this->options = $options;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from(env('MAIL_USERNAME'),'ATIWA FINANÇAS')
+                    ->subject($this->options['subject'])
+                    ->markdown($this->options['view'])
+                    ->with([
+                        'user' => $this->user
+                    ]);
+    }
+}
