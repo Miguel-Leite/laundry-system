9aa5f5b7c3b2543bb9f6bc57492cb8003faa60d8
Miguel Leite <miguelleite200leite@gmail.com>
2022-06-04 23:50:50 +0100

    ✨ add middleware Auth

---
+++ app/Http/Middleware/Auth.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Auth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!auth()->check() || auth()->user()->isAdmin < 0 || auth()->user()->isAdmin === null) {
+            return redirect()->route('pages.index')->with('danger','Não tens permissão de acessar está pagina!');
+        }
+        return $next($request);
+    }
+}
