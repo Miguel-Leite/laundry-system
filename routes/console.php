cc1e264c098be18f4afd374e2235e71202222465
Miguel Leite <miguelleite200leite@gmail.com>
2022-06-04 18:45:23 +0100

    ✅ add console for create user

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,24 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('user:create', function () {
+    $name = $this->ask('Nome completo?');
+    $email = $this->ask('E-mail?');
+    $address = $this->ask('Endereço?');
+    $isAdmin = $this->ask('Administrador?');
+    $employer = $this->ask('Funcionario?');
+    $pwd = $this->secret('Senha?');
+    // $pwd = $this->secret('Password?'); //
+    DB::table('users')->insert([
+            'name' => $name,
+            'email' => $email,
+            'address' => $address,
+            'isAdmin' => $isAdmin,
+            'isEmployee' => $employer,
+            'password' => bcrypt($pwd),
+            'created_at' => date_create()->format('Y-m-d H:i:s'),
+            'updated_at' => date_create()->format('Y-m-d H:i:s'),
+    ]);
+    $this->info('Account created for '.$name);
+});
