ac97fd2152c62cd570b219894fcaa4546da70c27
Miguel Leite <miguelleite200leite@gmail.com>
2022-06-05 17:28:14 +0100

    ✅ interface service

---
+++ app/Interfaces/IServices.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Interfaces;
+
+use App\Models\Service;
+use Illuminate\Http\Request;
+
+/**
+ * interface services
+ */
+interface IServices
+{
+    /**
+     * function create
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function create(Request $request);
+
+    /**
+     * function update
+     *
+     * @param Request $request
+     * @param integer $id
+     * @return void
+     */
+    public function update(Request $request, int $id);
+
+    /**
+     * function delete
+     *
+     * @param Service $service
+     * @return void
+     */
+    public function delete(Service $service);
+}
