1aaf05f6ba37b6802c6cb4fb7cd36e8c28fb02e3
Miguel Leite <miguelleite200leite@gmail.com>
2022-06-11 19:03:08 +0100

    🚀 add method getToWashs

--- app/Models/ToWash.php
+++ app/Models/ToWash.php
@@ -4,11 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class ToWash extends Model
 {
     use HasFactory;
     protected $table = 'to_washs';
     protected $fillable = ['start_date','end_date','returnned_to_the_client_date',
-            'total','total_paid','users_id','clothings_id','services_id'];
+            'total','total_paid','clients_id','clothings_id','services_id'];
+
+    public static function getToWashs()
+    {
+        return DB::select('SELECT to_washs.*, services.name as servicename,
+        clothings.name as clothingname, clients.name as clientname FROM to_washs, services, 
+        clothings, clients WHERE to_washs.clients_id = clients.id');
+    }
 }
