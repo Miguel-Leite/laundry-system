1d19b2c782639dc0eb886e40ac3fa97344ba4202
Miguel Leite <miguelleite200leite@gmail.com>
2022-06-11 08:22:54 +0100

    ✅ Controller Fabric CRUD

--- app/Http/Controllers/FabricController.php
+++ app/Http/Controllers/FabricController.php
@@ -10,16 +10,23 @@ class FabricController extends Controller implements IFabrics
 {
     public function create(Request $request)
     {
-        
+        $fabric = Fabric::create($request->all());
+        if ($fabric) return redirect()->back()->with('success','Tecido adicionado com sucesso!');
+        return redirect()->back()->with('danger','Não foi possível adicionar o tecido!');
     }
 
     public function update(Request $request, int $id)
     {
-        
+        $fabric = Fabric::find($id);
+        if (!$fabric) return redirect()->back()->with('danger','Não foi possível localizar o tecido!');
+        $fabric->update($request->all());
+        return redirect()->back()->with('success','Tecido actualizado com sucesso!');
     }
 
-    public function delete(Fabric $service)
+    public function delete(int $id)
     {
-        
+        $fabric = Fabric::find($id);
+        if (!$fabric) return redirect()->back()->with('danger','Não foi possível localizar o tecido!');
+        return redirect()->back()->with('success','Tecido removido com sucesso!');
     }
 }
