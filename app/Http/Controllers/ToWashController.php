cbbbcbc2b6507825989ee5f5ea43e590698da169
Miguel Leite <miguelleite200leite@gmail.com>
2022-06-11 19:05:09 +0100

    🚀 factoring code to create to washs

--- app/Http/Controllers/ToWashController.php
+++ app/Http/Controllers/ToWashController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Interfaces\IToWash;
+use App\Models\Client;
+use App\Models\Service;
 use App\Models\ToWash;
 use Illuminate\Http\Request;
 
@@ -10,9 +12,10 @@ class ToWashController extends Controller implements IToWash
 {
     public function create(Request $request)
     {
-        $towash = ToWash::create($request->all());
+        $data = $this->serializer_data($request->all());
+        $towash = ToWash::create($data);
         if ($towash) return redirect()->back()->with('success','Lavagem adicionado com sucesso!');
-        return redirect()->back()->with('danger','Não foi adicionar a lavagem!');
+        return redirect()->back()->with('danger','Não foi possível adicionar a lavagem!');
     }
 
     public function update(Request $request, int $id)
@@ -29,5 +32,16 @@ class ToWashController extends Controller implements IToWash
         if (!$towash) return redirect()->back()->with('danger','Não foi localizar a lavagem!');
         $towash->delete();
         return redirect()->back()->with('success','Lavagem removido com sucesso!');
+    }
+
+    public function serializer_data(array $data): array
+    {
+        $data = $data;
+        $data['clients_id']= Client::getClientClothingById($data['clothings_id'])->client_id;
+        $data['total'] = (int)Service::find($data['services_id'])->price;
+        $data['total_paid'] = (int)$data['total_paid'];
+        $data['clothings_id'] = (int)$data['clothings_id'];
+        $data['services_id'] = (int)$data['services_id'];
+        return $data;
     }
 }
