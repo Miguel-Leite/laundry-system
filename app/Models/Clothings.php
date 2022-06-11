5b1bc7a46d13292dec8dd9d6f53ee14529c6a07b
Miguel Leite <miguelleite200leite@gmail.com>
2022-06-11 19:03:58 +0100

    🚀 add method to return all clothings with client

--- app/Models/Clothings.php
+++ app/Models/Clothings.php
@@ -4,11 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Clothings extends Model
 {
     use HasFactory;
     protected $table = 'clothings';
     protected $fillable = ['name','description','color','size','iron',
-    'image','users_id','fabrics_id','categories_id'];
+    'image','clients_id','fabrics_id','categories_id'];
+
+    public static function getAllClothingsWithClient()
+    {
+        return DB::select("SELECT clothings.*, clients.name as clientname FROM clothings
+        INNER JOIN clients ON clothings.clients_id=clients.id");
+    }
 }
