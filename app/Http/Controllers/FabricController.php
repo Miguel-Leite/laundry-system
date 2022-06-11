16b2ed988e0299839959a6eae705a630a6ad6b7b
Miguel Leite <miguelleite200leite@gmail.com>
2022-06-11 08:06:06 +0100

    🚀 implement controller and interface and model fabrics

---
+++ app/Http/Controllers/FabricController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Interfaces\IFabrics;
+use App\Models\Fabric;
+use Illuminate\Http\Request;
+
+class FabricController extends Controller implements IFabrics
+{
+    public function create(Request $request)
+    {
+        
+    }
+
+    public function update(Request $request, int $id)
+    {
+        
+    }
+
+    public function delete(Fabric $service)
+    {
+        
+    }
+}
---
+++ app/Interfaces/IFabrics.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\Interfaces;
+
+use App\Models\Fabric;
+use Illuminate\Http\Request;
+
+/**
+ * interface IFabrics
+ */
+interface IFabrics
+{
+     /**
+     * function create
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function create(Request $request);
+
+    /**
+     * function update
+     *
+     * @param Request $request
+     * @param integer $id
+     * @return void
+     */
+    public function update(Request $request, int $id);
+
+    /**
+     * function delete
+     *
+     * @param Service $service
+     * @return void
+     */
+    public function delete(Fabric $service);
+}
